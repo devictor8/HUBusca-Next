63bf9f2c4b0d38ad08dcace9ca765911eed2e1ee
devictor8 <joaosalgado605@gmail.com>
2024-08-07 15:55:25 -0300

    Feat: NavBar Component adicionado ao Layout da rota '/'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import NavBar from "@/components/nav-bar/nav-bar";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <div>
-      <NavBar />
+    <div className={styles.container}>
+      <div>
+        <h1>HUBusca</h1>
+      </div>
     </div>
   );
 }
